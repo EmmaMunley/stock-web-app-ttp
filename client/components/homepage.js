6f2a098391ae86e426c5b2b45a920f8295af18e6
Emma Munley <emmamunley@gmail.com>
2020-03-03 02:45:06 -0500

    client setup

---
+++ client/components/homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+// import AllStocks from './all-stocks';
+
+const Homepage = () => {
+  return (
+    <div>
+      <p>Hello World</p>
+    </div>
+  );
+};
+
+export default Homepage;
---
+++ client/routes.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter, Route, Switch } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Homepage } from './components';
+import { me } from './store';
+
+class Routes extends Component {
+  componentDidMount() {
+    this.props.loadInitialData();
+  }
+
+  render() {
+    const { isLoggedIn } = this.props;
+
+    return (
+      <Switch>
+        {/* Routes placed here are available to all visitors */}
+        <Route exact path="/" component={Homepage} />
+        {/* <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} /> */}
+        {/* <Route exact path="/stocks" component={AllProducts} /> */}
+
+        {isLoggedIn && (
+          <Switch>
+            {/* Routes placed here are only available after logging in */}
+            <Route exact path="/" component={Homepage} />
+          </Switch>
+        )}
+      </Switch>
+    );
+  }
+}
+
+const mapState = state => {
+  return {
+    isLoggedIn: !!state.user.id,
+  };
+};
+
+const mapDispatch = dispatch => {
+  return {
+    loadInitialData() {
+      dispatch(me());
+    },
+  };
+};
+
+// The `withRouter` wrapper makes sure that updates are not blocked
+// when the url changes
+export default withRouter(connect(mapState, mapDispatch)(Routes));
+
+/**
+ * PROP TYPES
+ */
+Routes.propTypes = {
+  loadInitialData: PropTypes.func.isRequired,
+  isLoggedIn: PropTypes.bool.isRequired,
+};
